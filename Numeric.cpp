b6c25356fdaf59e37b8407afea72c3651e6ac15a
Aiukan <Ayka.Onky@gmail.com>
2023-05-30 17:43:07 +0300

    numeric differential

--- Numeric.cpp
+++ Numeric.cpp
@@ -1,2 +1,16 @@
 #pragma once
 
+#include "Numeric.h"
+
+namespace kana
+{
+	double differential(std::function<double(double)> f, double point)
+	{
+		return (f(point + EPSILON) - f(point)) / EPSILON;
+	}
+
+	double differential(std::function<std::complex<double>(std::complex<double>)> f, double point)
+	{
+		return f(std::complex<double>(point, EPSILON)).imag() / EPSILON;
+	}
+}
--- Numeric.h
+++ Numeric.h
@@ -1,7 +1,15 @@
 #pragma once
 
 #include <complex>
+#include <functional>
 
-namespace kana {
+namespace kana 
+{
 	const double PI = 3.14159265359;
+
+	const double EPSILON = 1e-9;
+
+	double differential(std::function<double(double)> f, double point);
+
+	double differential(std::function<std::complex<double>(std::complex<double>)> f, double point);
 }
