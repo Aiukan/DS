41df1c8f55ad039803d1184835a1302c4908b0e4
Aiukan <Ayka.Onky@gmail.com>
2023-05-30 05:46:03 +0300

    tr1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-    for (size_t i = 0; i < 10; i++)
+    for (size_t i = 0; i < 100; i++)
         std::cout << kana::random_uniform() * 100 << std::endl;
     std::string val;
     std::cin >> val;
